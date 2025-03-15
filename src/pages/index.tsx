15c27d677ff7f0d8794712de59e8398c82e3f2c5
wbdvlpmnt <jai.gokhale5219@gmail.com>
2025-03-15 14:38:33 -0500

    use a component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/header";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Welcome to the next todo app!</h1>
+    <div className="flex flex-row items-center justify-center p-4 ">
+      <Header />
     </div>
   );
 }
