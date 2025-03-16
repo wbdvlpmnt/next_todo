716d375472a92247b8b40993f610075343cedcc0
wbdvlpmnt <jai.gokhale5219@gmail.com>
2025-03-16 11:05:51 -0500

    add AppContext and AppProvider for state management

---
+++ src/context/appContext.tsx
@@ -0,0 +1,20 @@
+import React, { createContext, useState, ReactNode } from "react";
+
+interface AppContext {
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const AppContext = createContext<AppContext | undefined>(undefined);
+
+export const AppProvider = ({ children }: { children: ReactNode }) => {
+  const [value, setValue] = useState<string>("");
+
+  return (
+    <AppContext.Provider value={{ value, setValue }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export default AppContext;
