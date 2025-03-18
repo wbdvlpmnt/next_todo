9bba4d0fe77ce6527415a8a908dcb654a4473d98
wbdvlpmnt <jai.gokhale5219@gmail.com>
2025-03-18 10:38:11 -0500

    refactor: simplify Card and Header component styles by removing unnecessary border

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 
 export default function Card({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="w-full m-2 p-2 max-h-fit rounded-2xl border-2 border-amber-500">
-      {children}
-    </div>
-  );
+  return <div className="w-full m-2 p-2 max-h-fit rounded-2xl">{children}</div>;
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <h1 className="flex flex-row items-center justify-center text-4xl font-bold w-full border-amber-500 border-2 m-2 p-2">
+    <h1 className="flex flex-row items-center justify-center text-4xl font-bold w-full m-2 p-2">
       Next.js Todo App
     </h1>
   );
