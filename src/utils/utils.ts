00feb33c8f068fe1cf1de034567732c7e0e8ac60
wbdvlpmnt <jai.gokhale5219@gmail.com>
2025-03-25 15:29:47 -0500

    updated network request util

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,31 +1,60 @@
+async function getHandler(urlPath: string) {
+  const response = await fetch(urlPath);
+  return response;
+}
+
+async function postHandler(urlPath: string, method: string, body: any) {
+  if (body === undefined) {
+    throw new Error("POST request must have a body");
+  }
+  const response = await fetch(urlPath, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  return response;
+}
+
+function handleErrors(
+  response: Response,
+  urlPath: string,
+  method: string,
+  body: any
+) {
+  console.error("Network Request Error", {
+    urlPath,
+    method,
+    body,
+    response,
+  });
+}
+
 export async function networkRequest(
   urlPath: string,
   method: string,
   body?: any
 ) {
-  let response;
-  if (method === "GET") {
-    response = await fetch(urlPath);
-  } else if (method === "POST") {
-    if (body === undefined) {
-      throw new Error("POST request must have a body");
-    }
-    response = await fetch(urlPath, {
-      method: method,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    });
+  let response: Response | undefined;
+
+  switch (method) {
+    case "GET":
+      response = await getHandler(urlPath);
+      break;
+
+    case "POST":
+      response = await postHandler(urlPath, method, body);
+      break;
+
+    default:
+      throw new Error(`Unsupported HTTP method: ${method}`);
   }
+
   if (!response || (response.status !== 200 && response.status !== 201)) {
-    console.error("Network Request Error", {
-      urlPath,
-      method,
-      body,
-      response,
-    });
+    handleErrors(response, urlPath, method, body);
     return;
   }
+
   return response;
 }
